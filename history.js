faaf17605287a1fb721f82d9f9b4e25c9fdf50db
Netanel <you@example.com>
2022-08-09 22:07:46 +0300

    1

--- history.js
+++ history.js
@@ -13,119 +13,123 @@ const finishSection = document.getElementById("finish-section");
 //   const categoryQuestions = document.getElementById("category").value;       
   const difficultyQuestions = document.getElementById("difficulty").value;  
   const typeQuestions = document.getElementById("type").value;              
-  const historyApi = `https://opentdb.com/api.php?amount=50&category=23`; 
+  const historyApi = `https://opentdb.com/api.php?amount=10&category=23`; 
   
   function getQuestion() { 
-  fetch(historyApi)                               
-    .then((response) => response.json())    
-    .then((data) => {                       
-      dataQuestion = data;                  
-
-      if (typeQuestions === "multiple") {                                      
-        container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
-      } else {                                                                 
-        container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    
-      }
-    });
-}
-
-function desorderPositionArray(array, item) {               
-  let random = []                                          
-
-  while (random.length < item) {                            
-
-    let positionArray = Math.floor(Math.random() * item)    
-
-    if (!random.includes(array[positionArray])) {           
-      random.push(array[positionArray])                     
-    }
+    fetch(historyApi)                               
+      .then((response) => response.json())    
+      .then((data) => {                       
+        dataQuestion = data;                  
+  
+        if (typeQuestions === "multiple") {                                      
+          container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+        } else {                                                                 
+          container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    
+        }
+      });  setInterval(countDown,1000)  
   }
-  return random                                             
-}
+  
 
-
-function hiddeForm() {                                     
-  document.getElementById("form-cont").classList.add("dp-none");
-}
-
-
-
-
-function changeQuestion() {                                 
-
-  getInputsValue()                                          
-
-  if (position === dataQuestion.results.length - 1) {       
-    container.style.display = "none"                              
-    finishSection.innerHTML = `<div class = "finish" style="width: 100%; padding: 100px 0px; text-align: center; font-size: 1.3rem;"><h1 class = "mb">Test finished<br> Score = ${score}</h1><a href="../pages/select.html">Play Again</a></div>`
-    return
-  }
-
-  position++;                                                             
-  const typeQuestions = document.getElementById("type").value;            
-  if (typeQuestions === "multiple") {                                     
-    container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
-  } else {                                                                
-    container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   
-  }
-  return;
-
-}
-
-
-function renderQuestionsTemplate(question) {                 
-  var array = question[position].incorrect_answers           
-  array.push(question[position].correct_answer)              
-  let newOrderQuestion = desorderPositionArray(array, 4)  
-  setInterval(countDown,1000);    
-  return `<form class="form-test" onsubmit="event.preventDefault(), changeQuestion()">   
-    <div class="question-form">
-      <h2>${question[position].question}</h2>
-    </div>
-    <div class="answer-form">
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[0]}" required>
-        ${newOrderQuestion[0]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[1]}">
-        ${newOrderQuestion[1]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[2]}">
-        ${newOrderQuestion[2]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[3]}" >
-        ${newOrderQuestion[3]}
-      </label>
-      <button type="submit">
-        NEXT
-      </button>
-    </div>
-  </form>`;
-}
-
-setInterval(countDown,1000);
-function countDown() {
-    const minutes = Math.floor(timerCountDown)
-    let second = timeMin % 60;
-    if (second == '0' + second) {
-        timeHolder.innerHTML = `${minutes}:${second}`
-        timeMin -- ;
-    }
-} 
-
-
-                         
-function getInputsValue() {                                   
-  let answerInput = document.getElementsByTagName("input");   
-  answerInput = Array.from(answerInput);                     
-  answerInput.map(element => {                                
-    if (element.checked) {                                   
-      if (element.value === dataQuestion.results[position].correct_answer) {  
-        score = score + 10;                                  
+  
+  function desorderPositionArray(array, item) {               
+    let random = []                                          
+  
+    while (random.length < item) {                            
+  
+      let positionArray = Math.floor(Math.random() * item)    
+  
+      if (!random.includes(array[positionArray])) {           
+        random.push(array[positionArray])                     
       }
     }
-  })
-}
+    return random                                             
+  }
+  
+  
+  function hiddeForm() {                                     
+    document.getElementById("form-cont").classList.add("dp-none");
+  }
+  
+  
+  
+  
+  function changeQuestion() {                                 
+  
+    getInputsValue()                                          
+  
+    if (position === dataQuestion.results.length - 1) {       
+      container.style.display = "none"                              
+      finishSection.innerHTML = `<div class = "finish" style="width: 100%; padding: 100px 0px; text-align: center; font-size: 1.3rem;"><h1 class = "mb">Test finished<br> Score = ${score}</h1><a href="../pages/select.html">Play Again</a></div>`
+      return
+    }
+    position++;                                                             
+    const typeQuestions = document.getElementById("type").value;            
+    if (typeQuestions === "multiple") {                                     
+      container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+    } else {                                                                
+      container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   
+    }
+    
+    return;
+  
+  }
+  
+  
+  function renderQuestionsTemplate(question) {                 
+    var array = question[position].incorrect_answers           
+    array.push(question[position].correct_answer)              
+    let newOrderQuestion = desorderPositionArray(array, 4) 
+        
+    return `<form class="form-test" onsubmit="event.preventDefault(), changeQuestion()">   
+      <div class="question-form">
+        <h2>${question[position].question}</h2>
+      </div>
+      <div class="answer-form">
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[0]}" required>
+          ${newOrderQuestion[0]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[1]}">
+          ${newOrderQuestion[1]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[2]}">
+          ${newOrderQuestion[2]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[3]}" >
+          ${newOrderQuestion[3]}
+        </label>
+        <button type="submit">
+          NEXT
+        </button>
+      </div>
+    </form>`;
+  }
+  
+  
+  function countDown() {
+      const minutes = Math.floor(timerCountDown)
+      let second = timeMin % 60;
+      if (second == '0' + second) {
+          timeHolder.innerHTML = `${minutes}:${second}`
+          timeMin -- ;
+      }
+      if (second == 0) {
+        alert('you lost looser');
+      }
+      
+  } 
+                           
+  function getInputsValue() {                                   
+    let answerInput = document.getElementsByTagName("input");   
+    answerInput = Array.from(answerInput);                     
+    answerInput.map(element => {                                
+      if (element.checked) {                                   
+        if (element.value === dataQuestion.results[position].correct_answer) {  
+          score = score + 10;                                  
+        }
+      }
+    })
+  }
--- sport.js
+++ sport.js
@@ -13,123 +13,135 @@ const finishSection = document.getElementById("finish-section");
 //   const categoryQuestions = document.getElementById("category").value;       
   const difficultyQuestions = document.getElementById("difficulty").value;  
   const typeQuestions = document.getElementById("type").value;              
-  const sportApi = `https://opentdb.com/api.php?amount=50&category=21`;  
+  const sportApi = `https://opentdb.com/api.php?amount=30&category=21`; 
   
+ 
   function getQuestion() { 
-  fetch(sportApi)                               
-    .then((response) => response.json())    
-    .then((data) => {                       
-      dataQuestion = data;                  
-
-      if (typeQuestions === "multiple") {                                      
-        container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
-      } else {                                                                 
-        container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    
-      }
-    });
-}
-
-
-
-function desorderPositionArray(array, item) {               
-  let random = []                                          
-
-  while (random.length < item) {                            
-
-    let positionArray = Math.floor(Math.random() * item)    
-
-    if (!random.includes(array[positionArray])) {           
-      random.push(array[positionArray])                     
-    }
-  }
-  return random                                             
-}
-
-
-function hiddeForm() {                                     
-  document.getElementById("form-cont").classList.add("dp-none");
-}
-
-
-
-
-function changeQuestion() {                                 
-
-  getInputsValue()                                          
-
-  if (position === dataQuestion.results.length - 1) {       
-    container.style.display = "none"                              
-    finishSection.innerHTML = `<div class = "finish" style="width: 100%; padding: 100px 0px; text-align: center; font-size: 1.3rem;"><h1 class = "mb">Test finished<br> Score = ${score}</h1><a href="../pages/select.html">Play Again</a></div>`
-    return
-  }
-  position++;                                                             
-  const typeQuestions = document.getElementById("type").value;            
-  if (typeQuestions === "multiple") {                                     
-    container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
-  } else {                                                                
-    container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   
+    fetch(sportApi)                               
+      .then((response) => response.json())    
+      .then((data) => {                       
+        dataQuestion = data;                  
+  
+        if (typeQuestions === "multiple") {                                      
+          container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+        } else {                                                                 
+          container.innerHTML = renderQuestionsBoolean(dataQuestion.results);    
+        }
+      });  setInterval(countDown,1000)  
   }
   
-  return;
+  
 
-}
-
-
-function renderQuestionsTemplate(question) {                 
-  var array = question[position].incorrect_answers           
-  array.push(question[position].correct_answer)              
-  let newOrderQuestion = desorderPositionArray(array, 4) 
-      
-  return `<form class="form-test" onsubmit="event.preventDefault(), changeQuestion()">   
-    <div class="question-form">
-      <h2>${question[position].question}</h2>
-    </div>
-    <div class="answer-form">
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[0]}" required>
-        ${newOrderQuestion[0]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[1]}">
-        ${newOrderQuestion[1]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[2]}">
-        ${newOrderQuestion[2]}
-      </label>
-      <label>
-        <input type="radio" name="answer" value="${newOrderQuestion[3]}" >
-        ${newOrderQuestion[3]}
-      </label>
-      <button type="submit">
-        NEXT
-      </button>
-    </div>
-  </form>`;
-}
-
-
-function countDown() {
-    const minutes = Math.floor(timerCountDown)
-    let second = timeMin % 60;
-    if (second == '0' + second) {
-        timeHolder.innerHTML = `${minutes}:${second}`
-        timeMin -- ;
+  
+  function desorderPositionArray(array, item) {               
+    let random = []                                          
+  
+    while (random.length < item) {                            
+  
+      let positionArray = Math.floor(Math.random() * item)    
+  
+      if (!random.includes(array[positionArray])) {           
+        random.push(array[positionArray])                     
+      }
     }
-    if (second == 0) {
-      alert('you lost looser');
+    return random                                             
+  }
+
+  
+  
+  
+  function hiddeForm() {                                     
+    document.getElementById("form-cont").classList.add("dp-none");
+  }
+  
+  
+  
+  
+  
+  function changeQuestion() {                                 
+  
+    getInputsValue()                                          
+  
+    if (position === dataQuestion.results.length - 1) {       
+      container.style.display = "none"                              
+      finishSection.innerHTML = `<div class = "finish" style="width: 100%; padding: 100px 0px; text-align: center; font-size: 1.3rem;"><h1 class = "mb">Test finished<br> Score = ${score}</h1><a href="../pages/select.html">Play Again</a></div>`
+      return
+    }
+    position++;                                                             
+    const typeQuestions = document.getElementById("type").value;            
+    if (typeQuestions === "multiple") {                                     
+      container.innerHTML = renderQuestionsTemplate(dataQuestion.results);  
+    } else {                                                                
+      container.innerHTML = renderQuestionsBoolean(dataQuestion.results);   
     }
     
-} 
-                         
-function getInputsValue() {                                   
-  let answerInput = document.getElementsByTagName("input");   
-  answerInput = Array.from(answerInput);                     
-  answerInput.map(element => {                                
-    if (element.checked) {                                   
-      if (element.value === dataQuestion.results[position].correct_answer) {  
-        score = score + 10;                                  
+    return;
+  
+  }
+  
+  
+  function renderQuestionsTemplate(question) {                 
+    var array = question[position].incorrect_answers           
+    array.push(question[position].correct_answer)              
+    let newOrderQuestion = desorderPositionArray(array, 4) 
+    return `<form class="form-test" onsubmit="event.preventDefault(), changeQuestion()">   
+      <div class="question-form">
+        <h2>${question[position].question}</h2>
+      </div>
+      <div class="answer-form">
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[0]}" required>
+          ${newOrderQuestion[0]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[1]}">
+          ${newOrderQuestion[1]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[2]}">
+          ${newOrderQuestion[2]}
+        </label>
+        <label>
+          <input type="radio" name="answer" value="${newOrderQuestion[3]}" >
+          ${newOrderQuestion[3]}
+        </label>
+        <button type="submit">
+          NEXT
+        </button>
+      </div>
+    </form>`;
+  }
+  
+
+  
+  
+  function countDown() {
+      const minutes = Math.floor(timerCountDown)
+      let second = timeMin % 60;
+      if (second == '0' + second) {
+          timeHolder.innerHTML = `${minutes}:${second}`
+          timeMin -- ;
+          if (second == '0') {
+             alert('You Lost')
+             getQuestion()
+          }
+         
       }
-    }
-  })
-}
+  }
+
+  
+  
+   
+                           
+  function getInputsValue() {                                   
+    let answerInput = document.getElementsByTagName("input");   
+    answerInput = Array.from(answerInput);                     
+    answerInput.map(element => {                                
+      if (element.checked) {                                   
+        if (element.value === dataQuestion.results[position].correct_answer) {  
+          score = score + 10;                                  
+        }
+      }
+    })
+  }
+  
